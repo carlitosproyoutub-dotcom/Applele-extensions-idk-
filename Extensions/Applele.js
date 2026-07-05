56463f2af7bd97909e090328333279c90764d44d
dimix <299950422+Dimixixixiixix@users.noreply.github.com>
2026-07-05 04:13:33 -0700

    Update Applele.js

--- Extensions/Applele.js
+++ Extensions/Applele.js
@@ -5,6 +5,9 @@
       return {
         id: "applele",
         name: "Applele Blocks",
+        color1: "#ff0000",
+        color2: "#00ff00",
+        color3: "#00ff00",
         blocks: [
           {
             opcode: 'apple',
